5886f7883b4d4db24102999c8bb47b1704677d77
Aytekin Ozkan <aytekin7ozkan@gmail.com>
2025-05-16 15:40:07 +0300

    contact us section is done

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Navbar from '@/components/Navbar';
 import HomePage from '@/components/HomePage';
 import Benefits from '@/components/Benefits';
 import OurClasses from '@/components/OurClasses';
+import ContactUs from '@/components/ContactUs';
 
 export default function Home() {
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
@@ -29,6 +30,7 @@ export default function Home() {
       <HomePage setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
     </>
   );
 }
